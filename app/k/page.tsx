64cd640612744bdfdec5a17e1f917aa9a5f904ca
poudelsanchit <snchtpdl12@gmail.com>
2025-02-07 13:28:38 +0545

    delete 1 file and update 1 file

--- app/k/page.tsx
+++
@@ -1,9 +0,0 @@
-import KanbanBoard from "../app/components/KanbanBoard";
-
-export default function KanBan() {
-  return (
-    <div className="h-screen w-full  bg-[#0a0a0a] text-neutral-50 font-semibold">
-      <KanbanBoard />
-    </div>
-  );
-}
--- components/sidebar/nav-user.tsx
+++ components/sidebar/nav-user.tsx
@@ -79,23 +79,8 @@ export default function NavUser() {
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
               <DropdownMenuItem>
-                <Sparkles />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
                 <BadgeCheck />
                 Account
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCard />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Bell />
-                Notifications
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
